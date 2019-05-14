0040215e3083b9d64ada41d7c2d5596dea054166
mj <>
2019-05-14 14:48:51 +0900

    Bugfix_두번째로 수정한 버전 화살구현방식 변경됨/ 메모리 풀 사용 화살 모션 바꾸면서 조그만 것 들 수정함

--- BossReid/Assets/Script/ArrowBody.cs
+++ BossReid/Assets/Script/ArrowBody.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class ArrowBody : MonoBehaviour
 {
-    public GameObject Arrow;    // 복제할 미사일 오브젝트
-    public Transform ArrowLocation;   // 미사일이 발사될 위치
-    public float FireDelay;             // 미사일 발사 속도(미사일이 날아가는 속도x)
-    private bool FireState;             // 미사일 발사 속도를 제어할 변수
+    public GameObject Arrow;            // 복제할 화살 오브젝트
+    public Transform ArrowLocation;     // 화살이 발사될 위치
+    public float FireDelay;             // 화살 발사 속도(화살이 날아가는 속도x)
+    private bool FireState;             // 화살 발사 속도를 제어할 변수
+    public float arrowSpeed;
+
+    public int ArrowMaxPool;            //메모리 풀에 저장할 화살 개수
+    private MemoryPool MPool;           //메모리 풀
+    private GameObject[] ArrowArray;    //메모리 풀이랑 연동해서 사용할 화살 배열
+
+    Rigidbody rb;
+    Rigidbody arrowrb;
+
+
+    private void OnApplicationQuit()
+    {
+        //메모리 풀을 비웁니다
+        MPool.Dispose();
+    }
+
 
     void Start()
     {
         // 처음에 화살을 발사할 수 있도록 제어변수를 true로 설정
         FireState = true;
+
+        //메모리 풀을 초기화 
+        MPool = new MemoryPool();
+
+        //Arrow를 ArrowMaxPool만큼 생성
+        MPool.Create(Arrow, ArrowMaxPool);
+        // 배열도 초기화 (null값)
+        ArrowArray = new GameObject[ArrowMaxPool];
     }
 
     void Update()
@@ -21,7 +45,7 @@ public class ArrowBody : MonoBehaviour
         playerFire();
     }
 
-    // 미사일을 발사하는 함수
+    // 화살을 발사하는 함수
     private void playerFire()
     {
         // 제어변수가 true일때만 발동
@@ -33,21 +57,57 @@ public class ArrowBody : MonoBehaviour
                 // 코루틴 "FireCycleControl"이 실행되며
                 StartCoroutine(FireCycleControl());
                 // "Arrow"를 "ArrowLocation"의 위치에 "ArrowLocation"의 방향으로 복제한다.
-                 Instantiate(Arrow, ArrowLocation.position, ArrowLocation.rotation);
-              
 
+                GameObject s =Instantiate(Arrow, ArrowLocation.position, ArrowLocation.rotation);
+                s.GetComponent<ArrowMove>().dir = transform.right;
+                Debug.LogWarning(transform.right);
+
+                //  화살풀에서 발사되지 않은 화살을 찾아서 발사합니다.
+                for (int i = 0; i < ArrowMaxPool; i++)
+                {
+                    // 만약 화살배열[i]가 비어있다면
+                    if (ArrowArray[i] == null)
+                    {
+                        // 메모리풀에서 화살을 가져온다.
+                        ArrowArray[i] = MPool.NewItem();
+                        // 해당 화살의 위치를 화살 발사지점으로 맞춘다.
+                        ArrowArray[i].transform.position = ArrowLocation.transform.position;
+                        // 발사 후에 for문을 바로 빠져나간다.
+                        break;
+                    }
+                }
             }
         }
-    }
 
-    // 코루틴 함수
-    IEnumerator FireCycleControl()
-    {
-        // 처음에 FireState를 false로 만들고
-        FireState = false;
-        // FireDelay초 후에
-        yield return new WaitForSeconds(FireDelay);
-        // FireState를 true로 만든다.
-        FireState = true;
+
+        // 화살이 발사될때마다 화살을 메모리풀로 돌려보내는 것을 체크한다.
+        for (int i = 0; i < ArrowMaxPool; i++)
+        {
+            // 만약 화살[i]가 활성화 되어있다면
+            if (ArrowArray[i])
+            {
+                // 화살[i]의 Collider가 비활성 되었다면
+                if (ArrowArray[i].GetComponent<BoxCollider>().enabled == false)
+                {
+                    // 다시 Collider를 활성화 시키고
+                    ArrowArray[i].GetComponent<BoxCollider>().enabled = true;
+                    // 화살을 메모리로 돌려보내고
+                    MPool.RemoveItem(ArrowArray[i]);
+                    // 가리키는 배열의 해당 항목도 null(값 없음)로 만든다.
+                    ArrowArray[i] = null;
+                }
+            }
+        }
+
+        // 코루틴 함수
+        IEnumerator FireCycleControl()
+        {
+            // 처음에 FireState를 false로 만들고
+            FireState = false;
+            // FireDelay초 후에
+            yield return new WaitForSeconds(FireDelay);
+            // FireState를 true로 만든다.
+            FireState = true;
+        }
     }
 }
--- BossReid/Assets/Script/HeroMove.cs
+++ BossReid/Assets/Script/HeroMove.cs
@@ -1,24 +1,26 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class HeroMove : MonoBehaviour  {      public float moveSpeed = 2f;    //이동 속도     public float jumpPower = 3f;    //점프 속도       public int jumpCount = 2; //점프횟수    2를 3으로 바꾸면 3단 점프      new Rigidbody rigidbody;     Animator animator;      bool isGrounded = false;      bool isJumping;     Vector3 movement;      public static float horizontalMove;
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class HeroMove : MonoBehaviour  {      public float moveSpeed = 2f;    //이동 속도     public float jumpPower = 3f;    //점프 속도       public int jumpCount = 2; //점프횟수    2를 3으로 바꾸면 3단 점프      new Rigidbody rigidbody;     Animator animator;      bool isGrounded = false;      bool isMoving = false;
+    bool isJumping;      Vector3 movement;      public static float horizontalMove;     private Quaternion Up = Quaternion.identity;
 
-        void Awake()     {         rigidbody = GetComponent<Rigidbody>(); //컴포넌트를 불러옴         animator = GetComponent<Animator>(); //애니메이터 불러오기          jumpCount = 0;     }       private void OnCollisionEnter(Collision col)      {         if (col.gameObject.tag == "Ground")         {             isGrounded = true;    //Ground에 닿으면 isGround는 true             jumpCount = 2;          //Ground에 닿으면 점프횟수가 2로 초기화됨
-        }     }        void Update()      {
-        horizontalMove = Input.GetAxisRaw("Horizontal");
-        if(Input.GetButtonDown("Jump"))
+           void Awake()     {         rigidbody = GetComponent<Rigidbody>(); //컴포넌트를 불러옴         animator = GetComponent<Animator>(); //애니메이터 불러오기          jumpCount = 0;     }
+
+  
+
+        private void OnCollisionEnter(Collision col)      {          if (col.gameObject.tag == "Ground")         {             isGrounded = true;    //Ground에 닿으면 isGround는 true             jumpCount = 2;          //Ground에 닿으면 점프횟수가 2로 초기화됨
+        }     }        void Update()      {         MoveLeft();
+        MoveRight(); 
+
+
+        if (Input.GetButtonDown("Jump"))
         {          isJumping = true;         animator.SetTrigger("isJumping");         }         
          RunAnimationUpdate();         JumpAnimationUpdate();      }     void FixedUpdate()
     {
-        Run();         Jump();
+         Jump();        
     }
-     //----------------------     void Run()
-    {        if(horizontalMove >0) //왼쪽이동 
-        {             Vector3 scale = transform.localScale;             scale.x = Mathf.Abs(scale.x);             transform.localScale = scale;             movement.Set (horizontalMove,0,0);             movement = movement.normalized * moveSpeed * Time.deltaTime;             rigidbody.MovePosition(transform.position + movement);
-        }       else if(horizontalMove < 0) //오른쪽 이동 
-        {
-            Vector3 scale = transform.localScale;             scale.x = -Mathf.Abs(scale.x);             transform.localScale = scale;             movement.Set(horizontalMove, 0, 0);
-            movement = movement.normalized * moveSpeed * Time.deltaTime;
-            rigidbody.MovePosition(transform.position + movement);
-        }
-    }       void Jump()
+     //----------------------        public void MoveRight()     {         if (Input.GetKey(KeyCode.RightArrow))         {             if (transform.rotation.eulerAngles.y != 0)                      {                 Up.eulerAngles = new Vector3(0, 0, 0);                 transform.rotation = Quaternion.Slerp(transform.rotation, Up, 1);             }             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);             isMoving = true;         }      }
+ 
+    public void MoveLeft()     {         if (Input.GetKey(KeyCode.LeftArrow))         {             if (transform.rotation.eulerAngles.y != 180)             {                 Up.eulerAngles = new Vector3(0, 180, 0);                 transform.rotation = Quaternion.Slerp(transform.rotation, Up, 1);             }             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);             isMoving = true;         }         else         isMoving = false;      }
+
+    void Jump()
     {
         if(isGrounded)  //땅에 붙어있을 때
         {              if(jumpCount>0)  //점프 카운터가 남아 있으면
@@ -28,12 +30,12 @@
     } 
     void RunAnimationUpdate()
     {
-        if (horizontalMove == 0)
+        if (isMoving)
         {
-            animator.SetBool("isRunning", false);
+            animator.SetBool("isMoving", true);
         }         else
         {
-            animator.SetBool("isRunning", true);
+            animator.SetBool("isMoving", false);
         }
     }      void JumpAnimationUpdate()
     {
