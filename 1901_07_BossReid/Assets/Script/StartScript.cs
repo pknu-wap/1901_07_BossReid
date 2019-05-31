42e3d2abbd506d6cb8746d3af3bd7e3a06dbca9a
SoossFile <kimmins3483@gmail.com>
2019-06-01 01:18:39 +0900

    일단 시작씬 제작 완료

---
+++ 1901_07_BossReid/Assets/Script/StartScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour
+{
+    public void Click()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void isExit()
+    {
+        Application.Quit();
+    }
+}
