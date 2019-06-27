384b9949e99ffed74a5899368278271bd52bdb33
mj <>
2019-06-27 23:57:33 +0900

    AddThings_ 1.각 맵에 블럭 만듬

---
+++ 1901_07_BossReid/Assets/Script/IgnoreCollision_Plane.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollision_Plane : MonoBehaviour
+{
+	public GameObject Step;
+	public GameObject Demon;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Physics.IgnoreCollision(Step.GetComponent<Collider>(), Demon.GetComponent<Collider>());
+	}
+}
